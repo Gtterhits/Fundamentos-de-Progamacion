c7bf28e5f97e14fdd7f5e78bd9142c74542d75a7
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-07-12 23:10:03 -0500

    Add files via upload

---
+++ Numeros Binarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Numeros_Binarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables de entrada
+            Console.Write("Ingrese b0: ");
+            int b0 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese b1: ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese b2: ");
+            int b2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese b3: ");
+            int b3 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese b4: ");
+            int b4 = int.Parse(Console.ReadLine());
+
+            // Calculos de binario a entero
+            double B0 = b0 * Math.Pow(2, 4);
+            double B1 = b1 * Math.Pow(2, 3);
+            double B2 = b2 * Math.Pow(2, 2);
+            double B3 = b3 * Math.Pow(2, 1);
+            double B4 = b4 * Math.Pow(2, 0);
+
+            // Suma del numero para dar el entero
+            double Sumaent = B0 + B1 + B2 + B3 + B4;
+
+            // Resultados
+            Console.WriteLine("Numero de Binario a Entero es: " + Sumaent);
+        }
+    }
+}
---
+++ Triangulo doble 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Triangulo_doble_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // Valores de entrada de b, y, z
+            Console.Write("Ingrese valor de b en grados: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de z: ");
+            double z = double.Parse(Console.ReadLine());
+            // Angulo b de grados a radianes
+            double brad = b * (Math.PI / 180);
+
+            // Calculos
+            double t = Math.Sqrt(y * y + z * z);
+            double a = Math.Atan(y / z);
+            double d = Math.PI - (brad + a) - (Math.PI/2);
+            // Ley de seno
+            double x = (t * Math.Sin(brad)) / Math.Sin(d);
+
+            // Resultados
+            Console.WriteLine("El valor de x es: " + x);
+
+        }
+    }
+}
