1adaa00cfd71c4b45f214949f88c24e85d71016e
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-07-14 00:24:46 -0500

    Add files via upload

---
+++ Triangulo doble 3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Triangulo_doble_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Valores de entrada de d, b, y
+            Console.Write("Ingrese valor de w: ");
+            double w = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de d en grados: ");
+            double d = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de x: ");
+            double x = double.Parse(Console.ReadLine());
+            // Angulo d de grados a radianes
+            double drad = d * (Math.PI / 180);
+
+            // Calculos ley de cosenos y funciones trigonometricas
+            double t = Math.Sqrt(w * w + x * x - 2 * w * x * Math.Cos(drad));
+            double z = w * Math.Sin(drad);
+            double a = Math.Acos(z / t);
+            double y = t * Math.Sin(a);
+
+            // Resultados
+            Console.WriteLine("El valor de y es: " + y);
+        }
+    }
+}
---
+++ Triangulo doble 4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Triangulo_doble_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de las variables w, t, c
+            Console.Write("Ingrese el valor de w: ");
+            double w = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el valor de t: ");
+            double t = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el valor de c en grados: ");
+            double c = double.Parse(Console.ReadLine());
+            // Angulo c de grados a radianes
+            double crad = c * (Math.PI / 180);
+
+            // Calculos 
+            double e = 180 - c;
+            double erad = e * (Math.PI / 180);
+            double a = 180 - 90- c;
+            double arad = a * (Math.PI / 180);
+            double z = t * Math.Sin(crad);
+            double brad = Math.Acos(z / w) - arad;
+            // Ley de senos
+            double x = (w * Math.Sin(brad)) / Math.Sin(erad);
+
+            // Resultados
+            Console.WriteLine("EL valor de x es: " + x);
+        }
+    }
+}
