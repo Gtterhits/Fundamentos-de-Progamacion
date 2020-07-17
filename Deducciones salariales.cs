9d9186721c2e161bb794c2b7ba8e11601b92c9da
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-07-17 11:28:46 -0500

    Update Deducciones salariales.cs

--- Deducciones salariales.cs
+++ Deducciones salariales.cs
@@ -7,42 +7,43 @@ namespace Deducciones_salariales
         static void Main(string[] args)
         {
             // Programa para calcular deducciones salariales y su salario anual.
-            Console.WriteLine("Programa para calcular deducciones salariales y su salario anual.
+            Console.WriteLine("Programa para calcular deducciones salariales, salario real y salario anual.");
             // Entrada de salario mensual y su tipo de contrato.
             Console.WriteLine("Ingrese su salario mensual: ");
-            double sm = double.Parse(Console.ReadLine());
+            double salarioMensual = double.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese su tipo de contrato" + "Digite 1: Dependiente. / Digite 2: Independiente.");
-            int tc = int.Parse(Console.ReadLine());
+            int tipoContrato = int.Parse(Console.ReadLine());
             // Calculo de deducciones
-            double pr = 0, pen = 0, eps = 0, arl = 0, srm = 0, sa = 0;
-            double bc = sm * 0.40;
+            double procenRiesgo = 0, pension = 0, eps = 0, arl = 0, salarioReal = 0, salarioAnual = 0;
+            double ibc = salarioMensual * 0.40;
             double smmlv = 877803;
-            if (bc < smmlv) bc = smmlv;
-            if (tc == 1) {
-                pen = bc * 0.04;
-                eps = pen;
-                srm = sm - pen - eps;
-                sa = srm * 12 + sm;
+            if (ibc < smmlv) ibc = smmlv;
+            if (tipoContrato == 1) {
+                pension = ibc * 0.04;
+                eps = pension;
+                salarioReal = salarioMensual - pension - eps;
+                salarioAnual = salarioReal * 12 + salarioMensual;
             }
-            if (tc == 2){
+            if (tipoContrato == 2){
                 // Si y solo si es tipo de contrato independiente se debe de ingresar un numero del 1 al 5 que representa el porcentaje de riesgo.
                 Console.WriteLine("Ingrese un número de 1 a 5 equivalente a la clase del riesgo: ");
                 int cr = int.Parse(Console.ReadLine());
-                if (cr == 1) pr = 0.00522;
-                else if (cr == 2) pr = 0.01044;
-                else if (cr == 3) pr = 0.02436;
-                else if (cr == 4) pr = 0.04350;
-                else if (cr == 5) pr = 0.0696;
-                eps = bc * 0.125;
-                arl = bc * pr;
-                srm = sm - pen - eps - arl;
-                sa = srm * 12;
+                if (cr == 1) procenRiesgo = 0.00522;
+                else if (cr == 2) procenRiesgo = 0.01044;
+                else if (cr == 3) procenRiesgo = 0.02436;
+                else if (cr == 4) procenRiesgo = 0.04350;
+                else if (cr == 5) procenRiesgo = 0.0696;
+                eps = ibc * 0.125;
+                arl = ibc * procenRiesgo;
+                salarioReal = salarioMensual - pension - eps - arl;
+                salarioAnual = salarioReal * 12;
             }
-            Console.WriteLine("El calculo de deduccion de la Pension es: " + pen);
+            Console.WriteLine("El calculo de deduccion de la Pension es: " + pension);
             Console.WriteLine("El calculo de deduccion de la EPS es: " + eps);
             Console.WriteLine("El calculo de deduccion de el ARL es: " + arl);
-            Console.WriteLine("El salario real mensual es: " + srm);
-            Console.WriteLine("El salario anualcon deducciones es: " + sa);
+            Console.WriteLine("El salario real mensual es: " + salarioReal);
+            Console.WriteLine("El salario anualcon deducciones es: " + salarioAnual);
+            Console.WriteLine("...Fin del programa...");
         }
     }
 }
