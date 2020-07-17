460209f4d8fc42c5f1c89119b4074b18ba1fe68c
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-07-16 22:13:01 -0500

    Add files via upload

---
+++ Deducciones salariales.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Deducciones_salariales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa para calcular deducciones salariales y su salario anual.
+            Console.WriteLine("Programa para calcular deducciones salariales y su salario anual.");
+
+            // Entrada de salario mensual y su tipo de contrato.
+            Console.WriteLine("Ingrese su salario mensual: ");
+            double sm = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su tipo de contrato" + "Digite 1: Dependiente. / Digite 2: Independiente.");
+            int tc = int.Parse(Console.ReadLine());
+
+            // Si y solo si es tipo de contrato independiente se debe de ingresar un numero del 1 al 5 que representa el porcentaje de riesgo.
+            double pr = 0;
+            if (tc == 2)
+            {
+                Console.WriteLine("Ingrese un número de 1 a 5 equivalente a la clase del riesgo: ");
+                int cr = int.Parse(Console.ReadLine());
+                if (cr == 1)
+                {
+                    pr = 0.00522;
+                }
+                else if (cr == 2)
+                {
+                    pr = 0.01044;
+                }
+                else if (cr == 3)
+                {
+                    pr = 0.02436;
+                }
+                else if (cr == 4)
+                {
+                    pr = 0.04350;
+                }
+                else if (cr == 5)
+                {
+                    pr = 0.0696;
+                }
+            }
+            // Calculo de deducciones
+            double bc = sm * 0.40;
+            double smmlv = 877803;
+            if (bc < smmlv){
+                bc = smmlv;
+            }
+            if (tc == 1) {
+                double pen = bc * 0.04;
+                Console.WriteLine("El calculo de deduccion de la Pension es: " + pen);
+                double eps = pen;
+                Console.WriteLine("El calculo de deduccion de la EPS es: " + eps);
+                double srm = sm - pen - eps;
+                Console.WriteLine("El salario real mensual es: " + srm);
+                double sa = srm * 12 + sm;
+                Console.WriteLine("El salario anual con deducciones y prima es: " + sa);
+            }
+            if (tc == 2){
+                double pen = bc * 0.16;
+                Console.WriteLine("El calculo de deduccion de la Pension es: " + pen);
+                double eps = bc * 0.125;
+                Console.WriteLine("El calculo de deduccion de la EPS es: " + eps);
+                double arl = bc * pr;
+                Console.WriteLine("El calculo de deduccion de el ARL es: " + arl);
+                double srm = sm - pen - eps - arl;
+                Console.WriteLine("El salario real mensual es: " + srm);
+                double sa = srm * 12;
+                Console.WriteLine("El salario anualcon deducciones es: " + sa);
+            }
+        }
+    }
+}
---
+++ Sln ecuacion cuadratica.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sln_ecuacion_cuadratica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa que calcula la solucion de un ecuacion cuadratica de tipo ax^2 + bx + c.
+            Console.WriteLine("Programa que calcula la solucion de un ecuacion cuadratica de tipo ax^2+bx+c.");
+            // Entrada de variables a, b, c.
+            Console.WriteLine("Ingrese valor de a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese valor de b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese valor de c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            // Calculos
+            double dis = (b*b)-(4*a*c);
+            if (dis == 0) {
+                double x = -b / 2 * a;
+                Console.WriteLine("La solucion existe y es unica: " + x);
+            }
+            else if (dis>0){
+                double x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
+                double x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
+                Console.WriteLine("Existen 2 posibles soluciones.");
+                Console.WriteLine("x1 es: " + x1);
+                Console.WriteLine("x2 es: " + x2);
+            }
+            else if (dis < 0){
+                Console.WriteLine("No es posible calcular la solucion.");
+            }
+            Console.WriteLine("...Fin del programa...");
+        }
+    }
+}
