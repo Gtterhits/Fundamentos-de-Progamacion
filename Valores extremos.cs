6990fcd4d96acd295c1411a612333b857e3d9cbe
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-07-31 08:34:36 -0500

    Add files via upload

---
+++ Valores extremos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Valores_extremos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese número de datos (n): ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, max = 0, min = 0;
+            string usuarioMayor = "";
+            string usuarioMenor = "";
+            double total = 0;
+
+            while (i < n){
+                Console.WriteLine("Nombre de usuario: ");
+                string usuario = Console.ReadLine();
+                Console.WriteLine("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+                if (i == 0){
+                    max = edad;
+                    min = edad;
+                }
+                if (edad > max){
+                    max = edad;
+                    usuarioMayor = usuario;
+                }
+                if (edad < min){
+                    min = edad;
+                    usuarioMenor = usuario;
+                }
+                total += edad;
+                i++;
+            }
+            double promedio = total / n;
+            Console.WriteLine("La edad promedio es: " + promedio);
+            Console.WriteLine("Nombre de usuario: " + usuarioMayor + ". La edad maxima es: " + max);
+            Console.WriteLine("Nombre de usuario: " + usuarioMenor + ". La edad minima es: " + min);
+            Console.WriteLine("...Fin del programa...");
+        }
+    }
+}
