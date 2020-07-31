00f00b61ff6aa6c63382b04fa5b13d5865939179
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-07-31 10:11:08 -0500

    Add files via upload

---
+++ Juego Blackjack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Juego_Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("          $$$ Blackjack $$$");
+            Console.WriteLine("$$$ Empecemos el juego, Good Luck. $$$");
+            Random aleatorio = new Random();
+            int carta1 = aleatorio.Next(1, 11);
+            int carta2 = aleatorio.Next(1, 11);
+            Console.WriteLine("Carta 1: " + carta1);
+            Console.WriteLine("Carta 2: " + carta2);
+            int total = carta1 + carta2;
+            Console.WriteLine("Total: " + total);
+
+
+            string respuesta = "";
+            Console.WriteLine("Desea tomar una carta nueva? s/n");
+            respuesta = Console.ReadLine();
+            
+            while (respuesta == "s")
+            {
+
+                int nuevaCarta = aleatorio.Next(1, 11);
+                total += nuevaCarta;
+                Console.WriteLine("Su nueva carta es: " + nuevaCarta);
+                Console.WriteLine("Total: " + total);
+
+                Console.WriteLine("Desea tomar una carta nueva? s/n");
+                respuesta = Console.ReadLine();
+                
+            }
+        }
+    }
+}
