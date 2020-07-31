bbcc783cec2e93ce6c0cfe28b9782c1db198753b
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-07-31 10:56:12 -0500

    Update Juego Blackjack.cs

--- Juego Blackjack.cs
+++ Juego Blackjack.cs
@@ -8,6 +8,7 @@ namespace Juego_Blackjack
         {
             Console.WriteLine("          $$$ Blackjack $$$");
             Console.WriteLine("$$$ Empecemos el juego, Good Luck. $$$");
+            
             Random aleatorio = new Random();
             int carta1 = aleatorio.Next(1, 11);
             int carta2 = aleatorio.Next(1, 11);
@@ -15,15 +16,13 @@ namespace Juego_Blackjack
             Console.WriteLine("Carta 2: " + carta2);
             int total = carta1 + carta2;
             Console.WriteLine("Total: " + total);
-
-
+            
             string respuesta = "";
             Console.WriteLine("Desea tomar una carta nueva? s/n");
             respuesta = Console.ReadLine();
             
             while (respuesta == "s" && total <= 21)
             {
-
                 int nuevaCarta = aleatorio.Next(1, 11);
                 total += nuevaCarta;
                 Console.WriteLine("Su nueva carta es: " + nuevaCarta);
@@ -41,7 +40,6 @@ namespace Juego_Blackjack
                 {
                     Console.WriteLine("\n ¡Felicidades haz ganado el juego!"); respuesta = "n";
                 }
-
             }
             if (total < 21)
             {
