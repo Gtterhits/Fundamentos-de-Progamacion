f95fe5a7ddce95ebc459ff3970a82ab0f9d9fcc3
Sanemi <68073260+Gtterhits@users.noreply.github.com>
2020-08-16 14:00:27 -0500

    Add files via upload

---
+++ 09 Ciclos (simulacro p2 casa).cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _09_Ciclos__simulacro_p2_casa_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Juego de Dado 2!\nParte 2");
+
+            int puntos = 0, contadorVidas = 3, contadorTurnos = 1;
+            string respuesta = "";
+            Random aleatorio = new Random();
+            int dado1 = aleatorio.Next(1, 7);
+            //int dado1 = int.Parse(Console.ReadLine());
+            puntos += dado1;
+            Console.WriteLine("\nSu dado es: " + dado1);
+            Console.WriteLine("Total de puntos: " + puntos);
+            Console.WriteLine("Vidas: " + contadorVidas);
+
+            Console.WriteLine("\n¿Desea tirar un nuevo dado o desea retirarse?\n(s)/(n)");
+            respuesta = Console.ReadLine();
+
+            while (respuesta == "s" && puntos < 100) {
+                int dado2 = aleatorio.Next(1, 7);
+                //int dado2 = int.Parse(Console.ReadLine());
+                puntos += dado2;
+                contadorTurnos += 1;
+
+                if (contadorTurnos != 4) {
+                    Console.WriteLine("\nSu nuevo dado es: " + dado2);
+                    Console.WriteLine("Total de puntos: " + puntos);
+
+                    Console.WriteLine("Vidas: " + contadorVidas);
+                }
+                if (contadorTurnos == 2) {
+                    contadorVidas -= 1;
+                    Console.WriteLine("Vidas: " + contadorVidas);
+                    Console.WriteLine("\nHan pasado 2 turnos. Haz perdido una vida.");
+                }
+                if (contadorTurnos == 4) {
+                    int dado3 = aleatorio.Next(1, 7);
+                    //int dado3 = int.Parse(Console.ReadLine());
+                    int dado4 = aleatorio.Next(1, 7);
+                    //int dado4 = int.Parse(Console.ReadLine());
+                    int suma = dado3 + dado4;
+                    puntos += suma;
+                    Console.WriteLine("\nHan pasado 3 turnos. Puedes usar dos dados:\nDado 1: " + dado3 + "   Dado 2: " + dado4);
+                    Console.WriteLine("Total de puntos: " + puntos);
+                    Console.WriteLine("Vidas: " + contadorVidas);
+                    if (dado3 == dado4)  {
+                        contadorVidas += 1;
+                        Console.WriteLine("Sacaste un doble. Haz ganado una vida.");
+                        Console.WriteLine("Vidas: " + contadorVidas);
+                    }
+                    contadorTurnos = 0;
+                }
+                if (contadorVidas == 0) {
+                    Console.WriteLine("¡Haz perdido!"); break;
+                }
+                if (puntos >= 100) {
+                    Console.WriteLine("¡Felicidades haz ganado!"); break;
+                }          
+                Console.WriteLine("\n¿Desea tirar un nuevo dado o desea retirarse?\n(s)/(n)");
+                respuesta = Console.ReadLine();
+            }
+            if (respuesta == "n") {
+                Console.WriteLine("¡Te haz retirado!");
+                Console.WriteLine("¡Haz perdido!");
+            }
+        }
+    }
+}
---
+++ 09 Ciclos (simulacro p2).cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _09_Ciclos__simulacro_p2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Juego de Dado 2!");
+            int puntos = 0, contadorVidas = 3, contador1s = 0, contador6s = 0, dados12Caras = 2, dadoEspecial = 0, dado2 = 0;
+            string respuesta = "", respuestaDados = "";
+            Random aleatorio = new Random();
+
+            /*int dado1 = aleatorio.Next(1, 7);*/
+            int dado1 = int.Parse(Console.ReadLine());
+            puntos = dado1;
+            if (dado1 == 1) contador1s += 1;
+            if (dado1 == 6) contador6s += 1;
+            Console.WriteLine("\nSu dado es: " + dado1);
+            Console.WriteLine("Total de puntos: " + puntos);
+            Console.WriteLine("Vidas: " + contadorVidas);
+            
+            Console.WriteLine("\n¿Desea tirar un nuevo dado o desea retirarse?\n(s)/(n)");
+            respuesta = Console.ReadLine();
+
+            while (respuesta == "s" && puntos < 100)
+            {
+                if (dados12Caras != 0)
+                {
+                    Console.WriteLine("¿Desea usar un dado especial? " + "\nDados especiales: (" + dados12Caras + ")\n(s)/(n)");
+                    respuestaDados = Console.ReadLine();
+                    if (respuestaDados == "s")
+                    {
+                        dados12Caras -= 1;
+                        dadoEspecial = aleatorio.Next(1, 13);
+                        //dadoEspecial = int.Parse(Console.ReadLine());
+                        puntos += dadoEspecial;
+                        if (dadoEspecial == 1) contador1s += 1;
+                        if (dadoEspecial == 6) contador6s += 1;
+                        Console.WriteLine("\nSu dado especial es: " + dadoEspecial);
+                        Console.WriteLine("Total de puntos: " + puntos);
+                    }
+                    else
+                    {    
+                        int dado2 = aleatorio.Next(1, 7);
+                        //dado2 = int.Parse(Console.ReadLine());
+                        puntos += dado2;
+                        Console.WriteLine("\nSu nuevo dado es: " + dado2);
+                        Console.WriteLine("Total de puntos: " + puntos);
+                    }
+                }
+                else
+                {
+                    int dado2 = aleatorio.Next(1, 7);
+                    //dado2 = int.Parse(Console.ReadLine());
+                    puntos += dado2;
+                    Console.WriteLine("\nSu nuevo dado es: " + dado2);
+                    Console.WriteLine("Total de puntos: " + puntos);
+                }
+
+                if (respuesta == "s" && contador6s == 1 && dado2 == 6 && contadorVidas < 3)
+                {
+                    contadorVidas += 1;
+                    contador6s = 0;
+                }
+                if (dado2 == 1 || dadoEspecial == 1)
+                {
+                    contador1s += 1;
+                }
+                if (contador1s == 2)
+                {
+                    contadorVidas -= 1;
+                    puntos -= 10;
+                    contador1s = 0;
+                }
+                if (contadorVidas == 0)
+                {
+                    Console.WriteLine("¡Haz perdido!"); break;
+                }
+                if (dado2 == 6 || dadoEspecial == 6)
+                {
+                    contador6s += 1;
+                }
+                if (puntos >= 100)
+                {
+                    Console.WriteLine("¡Felicidades haz ganado!"); break;
+                }
+
+                Console.WriteLine("Vidas: " + contadorVidas);
+                Console.WriteLine("¿Desea tirar un nuevo dado o desea retirarse?\n(s)/(n)");
+                respuesta = Console.ReadLine();
+            }
+            if (respuesta == "n")
+            {
+                Console.WriteLine("¡Te haz retirado!");
+                Console.WriteLine("¡Haz perdido!");
+            }
+
+        }
+    }
+}
